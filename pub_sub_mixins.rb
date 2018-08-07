d902d56d215f2e4567b3a7a74c7095139185415e
Robert 808 <a.city.of.bridges@gmail.com>
2018-08-06 18:57:26 -0600

    Create Publisher and Subscriber mixin modules with stubs to outline future functionality

---
+++ pub_sub_mixins.rb
@@ -0,0 +1,19 @@
+module Publisher
+
+  def new_publisher_id
+  end
+
+  def notify
+  end
+end
+
+module Subscriber
+  def new_subscriber_id
+  end
+
+  def subscribe
+  end
+
+  def unsubscribe
+  end
+end
