e84a120c3c575348dc6ca1970210ac380a61de11
Robert 808 <a.city.of.bridges@gmail.com>
2018-08-06 18:52:50 -0600

    Create stubs for primary funcationality. Includes @@subscription-list, dispatch, and id_generator.

---
+++ event_bus.rb
@@ -0,0 +1,17 @@
+class EventBus
+
+  def self.subscriptions
+    @@subscriptions = []
+  end
+
+  def self.dispatch
+  end
+
+  def self.id_generator
+    def self.publisher
+    end
+    def self.subscriber
+    end
+  end
+
+end
