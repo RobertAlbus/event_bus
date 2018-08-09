7cba0efc0c1ad732a4f4a8d35e46ff79aeb5df9d
Robert 808 <a.city.of.bridges@gmail.com>
2018-08-08 22:13:26 -0600

    Add Event class with @payload{id: 1, message = 0}

--- event_bus.rb
+++ event_bus.rb
@@ -9,9 +9,7 @@ class EventBus
   def self.dispatch
   end
 
-
   ### ID GENERATOR ###
-
   @@event_agent_id = 0
   def self.event_agent_id
     @@event_agent_id
@@ -20,6 +18,27 @@ class EventBus
 
   def self.event_agent_id_generator
     @@event_agent_id += 1
-    return @@event_agent_id
+    @@event_agent_id
   end
+end
+
+class Event
+
+  attr_accessor :payload
+
+  def initialize( message = 0 )
+    @payload = {
+      id: Event.event_id_generator,
+      message: message
+    }
+  end
+
+  ### ID GENERATOR
+  @@event_id = 0
+  def self.event_id_generator
+    @@event_id += 1
+    @@event_id
+  end
+
+
 end
