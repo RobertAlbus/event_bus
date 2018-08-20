0b47bb0e9bc10b325640e79105e14a3b363dd086
Robert 808 <hire@robertalbus.com>
2018-08-19 20:06:39 -0600

    Add some thoughts for improving the application to application.rb

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 EventBus
 
-
-
+Event
 
 EventAgent
 
--- application.rb
+++ application.rb
@@ -2,3 +2,11 @@ require 'event_bus'
 require 'pub_sub_mixins'
 require 'test_models'
 EventBus.start_dispatcher
+
+
+#make Publisher.event_inbox not show up in the delivered payload
+#because this is messy when looking at Subscriber
+#maybe some core function in ruby for listing object ID instead of passing whole object
+#IE Object.object_id ??
+
+#create stub function for Subscriber.do_something_with_new_message
